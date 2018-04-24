8c93a71d3c8b0bb7c0df7c98e4e99aef908e4df6
Wen-New <16022742@myrp.edu.sg>
2018-04-24 12:04:41 +0800

    Toast

--- app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
@@ -2,8 +2,11 @@ package com.myapplicationdev.android.demolistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -12,12 +15,13 @@ public class MainActivity extends AppCompatActivity {
     ListView lv;
     ArrayAdapter aa;
     ArrayList<Food> food;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lv = (ListView)this.findViewById(R.id.lvFood);
+        lv = (ListView) this.findViewById(R.id.lvFood);
 
         // Create a few food objects in Food array
         food = new ArrayList<Food>();
@@ -29,5 +33,18 @@ public class MainActivity extends AppCompatActivity {
         // each row and the food String array together
         aa = new FoodAdapter(this, R.layout.row, food);
         lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Food selectedFood = food.get(position);
+
+                Toast.makeText(MainActivity.this, selectedFood.getName()
+                                + " Star: " + selectedFood.isStar(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
+
 }
